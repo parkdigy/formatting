9c87dd9ab381dfc4e6d98a688b4e4ce024d4f418
parkdigy <parkdigy@gmail.com>
2025-12-10 22:14:15 +0900

    1.0.6

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -4,10 +4,9 @@ import dayjs from'dayjs';/******************************************************
  * @param allowCharacters 허용할 문자들 (기본값: '*')
  * @returns 하이픈이 추가된 사업자등록번호
  * ******************************************************************************************************************/
-function formatBusinessNo(v, allowCharacters) {
-    if (allowCharacters === void 0) { allowCharacters = '*'; }
-    var str = v.replace(new RegExp("[^0-9".concat(allowCharacters, "]"), 'g'), '');
-    var values = [str.slice(0, 3)];
+function formatBusinessNo(v, allowCharacters = '*') {
+    const str = v.replace(new RegExp(`[^0-9${allowCharacters}]`, 'g'), '');
+    const values = [str.slice(0, 3)];
     if (str.length > 3)
         values.push(str.slice(3, 5));
     if (str.length > 5)
@@ -36,30 +35,29 @@ function formatDate(date, format) {
  * @returns 콤마 추가된 문자열
  * ******************************************************************************************************************/
 function formatNumber(v) {
-    var strValue = String(v)
+    const strValue = String(v)
         .trim()
         .replace(/[^\d.+-]/g, '');
-    var match = strValue.match(/^([+-]?)(\d+)(\.\d+)?$/);
+    const match = strValue.match(/^([+-]?)(\d+)(\.\d+)?$/);
     if (!match)
         return strValue;
-    var sign = match[1];
-    var intPart = match[2];
-    var decimalPart = match[3] || '';
+    const sign = match[1];
+    const intPart = match[2];
+    let decimalPart = match[3] || '';
     if (/^\.0+$/.test(decimalPart)) {
         decimalPart = '';
     }
-    var formattedInt = Number(intPart).toLocaleString();
-    return "".concat(sign).concat(formattedInt).concat(decimalPart);
+    const formattedInt = Number(intPart).toLocaleString();
+    return `${sign}${formattedInt}${decimalPart}`;
 }/********************************************************************************************************************
  * 주민등록번호에 하이픈 추가하는 함수
  * @param v 주민등록번호
  * @param allowCharacters 허용할 문자들 (기본값: '*')
  * @returns 하이픈 추가된 주민등록번호
  * ******************************************************************************************************************/
-function formatPersonalNo(v, allowCharacters) {
-    if (allowCharacters === void 0) { allowCharacters = '*'; }
-    var str = v.replace(new RegExp("[^0-9".concat(allowCharacters, "]"), 'g'), '');
-    var values = [str.slice(0, 6)];
+function formatPersonalNo(v, allowCharacters = '*') {
+    const str = v.replace(new RegExp(`[^0-9${allowCharacters}]`, 'g'), '');
+    const values = [str.slice(0, 6)];
     if (str.length > 6)
         values.push(str.slice(6));
     return values.join('-');
@@ -69,37 +67,36 @@ function formatPersonalNo(v, allowCharacters) {
  * @param allowCharacters 허용할 문자들 (기본값: '*')
  * @returns 하이픈 추가된 전화번호
  * ******************************************************************************************************************/
-function formatTelNo(v, allowCharacters) {
-    if (allowCharacters === void 0) { allowCharacters = '*'; }
+function formatTelNo(v, allowCharacters = '*') {
     if (v === undefined || v === null)
         return v;
-    var escapedAllowChars = allowCharacters.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
-    var filterRegex = new RegExp("[^0-9+".concat(escapedAllowChars, "]"), 'g');
-    var str = v.replace(filterRegex, '');
-    var isLastDash = v.endsWith('-');
+    const escapedAllowChars = allowCharacters.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const filterRegex = new RegExp(`[^0-9+${escapedAllowChars}]`, 'g');
+    const str = v.replace(filterRegex, '');
+    const isLastDash = v.endsWith('-');
     if (str[0] !== '0' && !['15', '16', '18'].includes(str.slice(0, 2))) {
         return str;
     }
-    var preLen = 3;
+    let preLen = 3;
     if (str.startsWith('02'))
         preLen = 2;
     else if (['15', '16', '18'].includes(str.slice(0, 2)))
         preLen = 4;
-    var tmp = str;
+    let tmp = str;
     if (['15', '16', '18'].includes(str.slice(0, 2))) {
         if (str.length > preLen && str.length <= preLen + 4) {
-            tmp = "".concat(str.slice(0, preLen), "-").concat(str.slice(preLen));
+            tmp = `${str.slice(0, preLen)}-${str.slice(preLen)}`;
         }
     }
     else if (str.length > preLen) {
         if (str.length <= preLen + 3) {
-            tmp = "".concat(str.slice(0, preLen), "-").concat(str.slice(preLen));
+            tmp = `${str.slice(0, preLen)}-${str.slice(preLen)}`;
         }
         else if (str.length <= preLen + 7) {
-            tmp = "".concat(str.slice(0, preLen), "-").concat(str.slice(preLen, preLen + 3), "-").concat(str.slice(preLen + 3));
+            tmp = `${str.slice(0, preLen)}-${str.slice(preLen, preLen + 3)}-${str.slice(preLen + 3)}`;
         }
         else if (str.length <= preLen + 8) {
-            tmp = "".concat(str.slice(0, preLen), "-").concat(str.slice(preLen, preLen + 4), "-").concat(str.slice(preLen + 4));
+            tmp = `${str.slice(0, preLen)}-${str.slice(preLen, preLen + 4)}-${str.slice(preLen + 4)}`;
         }
     }
     if (isLastDash && str.length === preLen) {
@@ -113,9 +110,9 @@ function formatTelNo(v, allowCharacters) {
  * @param l 총 항목 수 (3=x.x.x, 5=x.x.x.x.x)
  * ******************************************************************************************************************/
 function formatVersion(v, vl, l) {
-    var va = v.toString().split('.');
-    var vsa = [];
-    for (var i = 0; i < l; i += 1) {
+    const va = v.toString().split('.');
+    const vsa = [];
+    for (let i = 0; i < l; i += 1) {
         if (va.length >= i + 1) {
             vsa.push(va[i].padStart(vl, '0'));
         }
